6c353b8afc00bafe2d0bc7c41a57b64c5b53d9cf
sanjeevraj-belsterns <sanjeev.r@belsterns.com>
2025-04-05 14:22:00 +0530

    Update app.js

--- src/app.js
+++ src/app.js
@@ -26,7 +26,7 @@ function writeTasks(tasks) {
 // Get all tasks
 app.get('/tasks', (req, res) => {
     const tasks = readTasks();
-    console.log("---!tasks Listed!---");
+    console.log("---tasks Listed---");
     console.log(tasks);
     res.json(tasks);
 });
