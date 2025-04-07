73160a352da321de2c97199d2ffd2659f5f9cc5e
EZTest AI <eztest.ai@commit.com>
2025-04-07 09:50:42 +0530

    Added src/__tests__/jest.config.js file

---
+++ src/__tests__/jest.config.js
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: 'node',
+  roots: ['<rootDir>/src'],
+  testRegex: '(/__tests__/.*|(\.|/)(test|spec))\.jsx?$',
+  moduleFileExtensions: ['js', 'jsx', 'json', 'node'],
+};
